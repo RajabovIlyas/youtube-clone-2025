9cd60b5abe81bcfd999a175e466d547c4817cdf5
ilyas <ilyasrajabow@gmail.com>
2025-12-23 20:12:12 +0500

    check to build

--- src/pages/watch-page/WatchPage.jsx
+++ src/pages/watch-page/WatchPage.jsx
@@ -1,17 +1,34 @@
 import { useSearchParams } from 'react-router';
 import Header from '../../widgets/header';
+import { getVideoByIdUrl } from '../../features/api';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const WatchPage = () => {
+  const [video, setVideo] = useState(null);
   const [searchParams] = useSearchParams();
+
+  const fetchVideo = async () => {
+    const response = await axios.get(getVideoByIdUrl(videoId));
+    setVideo(response.data);
+  };
+
+  const videoId = searchParams.get('v');
+
+  useEffect(() => {
+    if (!videoId) return;
+
+    fetchVideo();
+  }, []);
 
   return (
     <>
     <Header />
     <div>
-      <iframe
+      <iframe className='w-[80vw] h-[80vh]'
         width="420"
         height="315"
-        src={`https://www.youtube.com/embed/${searchParams.get('v')}`}
+        src={`https://www.youtube.com/embed/${videoId}`}
       ></iframe>
     </div>
     </>
